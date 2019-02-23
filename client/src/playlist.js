8ba8527d364a38b21ac851269e6fa25ad3c91d2e
aElian01 <eliana.sur@gmail.com>
2019-02-23 00:20:56 -0800

    revised to use axios for fetching data

--- client/src/playlist.js
+++ client/src/playlist.js
@@ -1,20 +1,23 @@
+const axios = require('axios');
 let playlist = [];
 
 const createPlaylist = function (callback) {
-  fetch('http://localhost:3001/playlist')
-    .then(res => res.json())
+  axios.get('http://localhost:3001/playlist')
+    .then(res => {
+      return res.data;
+    })
     .then(
       (result) => {
         for (var i = 0; i < result.length; i++) {
-          playlist.splice(0, 0, result[i]);
+          playlist.push(result[i]);
         }
         callback();
-      },
+      })
+    .catch(
       (error) => {
         callback(error);
         console.log(error, 'ERROR POPULATING PLAYLIST!');
-      }
-    );
+      });
 };
 
 module.exports = {
