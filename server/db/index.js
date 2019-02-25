7f640dff04b6b267d1edd5cc84ec7a656b11baaf
aElian01 <eliana.sur@gmail.com>
2019-02-24 19:48:24 -0800

    pick right port

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 dbConnection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME ? process.env.RDS_HOSTNAME : 'localhost',
-  user: process.env.RDS_USERNAME ? process.env.RDS_USERNAME : 'root',
-  password: process.env.RDS_PASSWORD ? process.env.RDS_PASSWORD : '',
+  host: process.env.RDS_HOSTNAME || 'localhost',
+  user: process.env.RDS_USERNAME || 'root',
+  password: process.env.RDS_PASSWORD || '',
   database: 'playlist'
 });
 
