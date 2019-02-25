ed66012f04a4aabd82c905475b9b8177befc9160
aElian01 <eliana.sur@gmail.com>
2019-02-24 23:33:17 -0800

    jest and enzyme tests for Player component

--- client/src/components/Player.test.js
+++ client/src/components/Player.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Player from './Player';
 
 describe('Player', () => {
   it('should render correctly', () => {
     const component = shallow(<Player />);
-
     expect(component).toMatchSnapshot();
   });
+
+  it('should render metaData', () => {
+    const wrap = mount(<Player />);
+    wrap.update();
+    expect(wrap).toMatchSnapshot();
+    wrap.unmount();
+  });
 });
+
